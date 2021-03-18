d5abe1c00af8a85549615e96631e45c21b81a7df
sam <weaponz326@gmail.com>
2021-03-18 13:03:52 +0000

    made all sections of event page dynamic

--- src/app/pages/event-page/event-page.component.ts
+++ src/app/pages/event-page/event-page.component.ts
@@ -49,11 +49,11 @@ export class EventPageComponent implements OnInit, AfterViewInit {
           this.dataContent = res;
           this.eventContent = this.dataContent.event[0];
           this.speakersContent = this.dataContent.hosts;
-          // this.scheduleContent = this.dataContent.tickets;
+          this.scheduleContent = this.dataContent.schedule;
           this.pricingContent = this.dataContent.tickets;
           this.organisersContent = this.dataContent.organizers;
           this.sponsorsContent = this.dataContent.sponsors;
-          console.log(this.sponsorsContent);
+          console.log(this.sponsorsContent.length);
           
           this.description = this.eventContent.description;
           this.eventTitle = this.eventContent.title;
