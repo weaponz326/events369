36f8c4c106ffe6063df333995058ee285fba0cfa
David <davidkofiahensackey@gmail.com>
2021-03-23 18:04:46 +0000

    Dark Theme Mode for Events369 from toggle button

--- src/app/components/account-side-menu/account-side-menu.component.ts
+++ src/app/components/account-side-menu/account-side-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeSwitcherService } from 'src/app/services/theme-switcher.service';
 
 @Component({
   selector: 'app-account-side-menu',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSideMenuComponent implements OnInit {
 
-  constructor() { }
+  dark_theme: boolean = false
+
+  constructor(
+    private themeSwitcher: ThemeSwitcherService 
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  
+
+  switchTheme() {
+    
+    if(!this.dark_theme) {
+      console.log(document.body.setAttribute('class', 'dark-theme'))
+      this.dark_theme = true;
+
+      
+    } else {
+      console.log(document.body.removeAttribute('class'))
+      this.dark_theme = false;
+
+    }
+    
+    
+  
   }
 
 }
