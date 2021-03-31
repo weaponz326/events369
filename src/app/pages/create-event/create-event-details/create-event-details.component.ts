db0714c7ac1fee8aab256b204deccdcbf618f9c3
sam <weaponz326@gmail.com>
2021-03-31 15:40:38 +0000

    event details

--- src/app/pages/create-event/create-event-details/create-event-details.component.ts
+++ src/app/pages/create-event/create-event-details/create-event-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { EventDetailsService } from 'src/app/services/event-details/event-details.service';
 
 @Component({
   selector: 'app-create-event-details',
@@ -9,12 +11,22 @@ import { Router } from '@angular/router';
 export class CreateEventDetailsComponent implements OnInit {
 
   isLoading: boolean;
+  saved: boolean;
+  form: FormGroup = new FormGroup({});
+  imgSrc: string;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private eventDetailsService: EventDetailsService
+  ) {
     this.isLoading = false;
+    this.saved = false;
+    this.imgSrc = '../../../../assets/images/placeholder-image.png';
   }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
   previous() {
@@ -26,6 +38,61 @@ export class CreateEventDetailsComponent implements OnInit {
     setTimeout(() => {
       this.router.navigateByUrl('/create_event/ticketing');
     }, 3500);
+  }
+
+  public get f(): any {
+    return this.form.controls;
+  }
+  
+  initForm(): void {
+    this.form = this.formBuilder.group({
+      banner: ['', Validators.required],
+    });
+  }
+
+  create(): void {
+    this.saved = true;
+    if (this.form.valid) {
+      console.log('form is valid');
+      this.isLoading = true;
+      // this.eventDetailsService.createEventDetails(this.getFormData()).then(
+      //   res => {
+      //     if (res) {
+      //       this.isLoading = false;
+      //       this.router.navigateByUrl('/create_event/organizers');
+      //     }
+      //     else {
+      //       this.isLoading = false;
+      //       alert('didnt create');
+      //     }
+      //   },
+      //   err => {
+      //     console.log(err);
+      //     this.isLoading = false;
+      //   }
+      // );
+    }
+  }
+
+  onFileSelected(e: any){
+    const file:File = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("thumbnail", file);
+
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e: any) => {
+        this.imgSrc = e.target.result;
+      }
+    }
+  }
+
+  getFormData(): any {
+    const data = {
+      banner: this.f.banner.value,
+    };
+    return data;
   }
 
 }
