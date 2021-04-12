a08dcbbc45bc81ef8165b352891e0a1470fd5e92
David <davidkofiahensackey@gmail.com>
2021-04-12 16:19:14 +0000

    done with edit basic info data binding to sessionData (left with venue_tobe_announced)

--- src/app/pages/edit-event/edit-event-schedules/edit-event-schedules.component.ts
+++ src/app/pages/edit-event/edit-event-schedules/edit-event-schedules.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { SchedulingService } from 'src/app/services/scheduling/scheduling.service';
+import { DatetimeFormatterService } from 'src/app/services/datetime-formatter/datetime-formatter.service';
+import { EventSideMenuCheckService } from 'src/app/services/event-side-menu-check/event-side-menu-check.service';
 
 @Component({
   selector: 'app-edit-event-schedules',
@@ -9,24 +13,69 @@ import { Router } from '@angular/router';
 export class EditEventSchedulesComponent implements OnInit {
 
   isLoading: boolean;
-  
+  saved: boolean;
+  form: FormGroup = new FormGroup({});
+
+  isDaily: boolean;
+  isWeekly: boolean;
+  isMonthly: boolean;
+
+  eventId: string = ''
   eventTitle: string = ''
-  eventDate: string = ''
+  eventDate: string = '' 
+  startDate: string = ''
+  endDate: string = ''
+  eventRecurs: string = ''
+  eventOccursEvery: string = ''
   
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private schedulingService: SchedulingService,
+    private dtService: DatetimeFormatterService,
+    private checkSessionData: EventSideMenuCheckService
+    ) {
     this.isLoading = false;
+    this.saved = false; 
+    this.isDaily = true;
+    this.isWeekly = false;
+    this.isMonthly = false; 
   }
 
   ngOnInit(): void {
-    this.router.routeReuseStrategy.shouldReuseRoute = function () {
-      return false;
-    };
+    // this.router.routeReuseStrategy.shouldReuseRoute = function () {
+    //   return false;
+    // };
     
     var data: any =  sessionStorage.getItem('created_event')
     data = JSON.parse(data)
+    this.eventId = data.event[0].id;
     this.eventTitle = data.event[0].title;
-    this.eventDate = data.event[0].start_date_time
+    this.startDate = data.event[0].start_date_time;
+    this.endDate = data.event[0].end_date_time;
+    this.eventRecurs = data.schedule[0].recurs;
+    this.eventOccursEvery = data.schedule[0].occurs_every;
+
+    this.initForm();
     // console.log(data.event[0].title)
+  }
+
+  
+  public get f(): any {
+    return this.form.controls;
+  }
+
+  initForm(): void {
+    this.form = this.formBuilder.group({
+      start_date: [this.startDate],
+      end_date: [this.endDate],
+      recurs: [this.eventRecurs, Validators.required],
+      days: [this.eventOccursEvery],
+      months: [''],
+    });
+
+    this.form.controls['start_date'].disable();
+    this.form.controls['end_date'].disable();
   }
 
   previous() {
@@ -38,6 +87,80 @@ export class EditEventSchedulesComponent implements OnInit {
     setTimeout(() => {
       this.router.navigateByUrl('/create_event/more_details');
     }, 3500);
+  }
+
+  edit(): void {
+    this.saved = true;
+    if (this.form.valid) {
+      console.log('form is valid');
+      this.isLoading = true;
+      this.schedulingService.createSchedule(this.getFormData()).then(
+        res => {
+          if (res) {
+            console.log(res);
+            this.isLoading = false;
+            this.getCreatedEvent(this.eventId)
+
+            if(this.checkSessionData.eventHasMoreDetailsData()) {
+              this.router.navigateByUrl('/edit_event/more_details');
+
+            } else {
+              this.router.navigateByUrl('/create_event/more_details');
+            }
+
+          }
+          else {
+            this.isLoading = false;
+            alert('didnt create');
+          }
+        },
+        err => {
+          console.log(err);
+          this.isLoading = false;
+        }
+      );
+    }
+  }
+
+  getFormData(): any {
+    const data = {
+      event_id: this.eventId,
+      recurs: this.f.recurs.value,
+      occurs_every: this.f.recurs.daily,
+    };
+    return data;
+  } 
+
+  setReccurance(){
+    console.log(this.f.recurs.value);
+    if (this.f.recurs.value == 0) {
+      this.isDaily = true;
+      this.isWeekly = false;
+      this.isMonthly = false;
+    }
+    else if (this.f.recurs.value == 1) {
+      this.isDaily = true;
+      this.isWeekly = true;
+      this.isMonthly = false;
+    }
+    else if (this.f.recurs.value == 2) {
+      this.isDaily = true;
+      this.isWeekly = true;
+      this.isMonthly = true;
+    }    
+  }
+
+  
+  getCreatedEvent(eventId: any): void {
+    this.schedulingService.getCreatedEvent(eventId).then(
+      res => {
+        console.log(res);
+        sessionStorage.setItem('created_event', JSON.stringify(res));
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
