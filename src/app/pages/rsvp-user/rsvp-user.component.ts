74a8acd52c60a9d873f7423869bb8566c27911f4
Weaponz <weaponz326@gmail.com>
2021-07-28 10:27:54 +0000

    form fields selection

--- src/app/pages/rsvp-user/rsvp-user.component.ts
+++ src/app/pages/rsvp-user/rsvp-user.component.ts
@@ -8,6 +8,23 @@ import { RsvpService } from 'src/app/services/rsvp/rsvp.service';
 })
 export class RsvpUserComponent implements OnInit {
 
+  rsvpData: any;
+
+  isPrefixIncluded: boolean = false;
+  isPrefixRequired: boolean = false;
+  isFirstNameRequired: boolean = false;
+  isFirstNameIncluded: boolean = false;
+  isLastNameIncluded: boolean = false;
+  isLastNameRequired: boolean = false;
+  isGenderIncluded: boolean = false;
+  isGenderRequired: boolean = false;
+  isEmailIncluded: boolean = false;
+  isEmailRequired: boolean = false;
+  isPhoneIncluded: boolean = false;
+  isPhoneRequired: boolean = false;
+  isAddressIncluded: boolean = false;
+  isAddressRequired: boolean = false;
+
   constructor(private rsvp: RsvpService) { }
 
   ngOnInit(): void {
@@ -18,6 +35,38 @@ export class RsvpUserComponent implements OnInit {
     this.rsvp.getRsvp().then(
       res => {
         console.log(res);
+        this.rsvpData = res[0].form_fields;
+
+        this.rsvpData.forEach((data: any) => {
+          if(data.field_name == "Prefix"){
+            this.isPrefixIncluded = true;
+            this.isPrefixRequired = data.required;
+          }
+          if(data.field_name == "First Name"){
+            this.isFirstNameIncluded = true;
+            this.isFirstNameRequired = data.required;
+          }
+          if(data.field_name == "Last Name"){
+            this.isLastNameIncluded = true;
+            this.isLastNameRequired = data.required;
+          }
+          if(data.field_name == "Gender"){
+            this.isGenderIncluded = true;
+            this.isGenderRequired = data.required;
+          }
+          if(data.field_name == "Email"){
+            this.isEmailIncluded = true;
+            this.isEmailRequired = data.required;
+          }
+          if(data.field_name == "Phone No."){
+            this.isPhoneIncluded = true;
+            this.isPhoneRequired = data.required;
+          }
+          if(data.field_name == "Address"){
+            this.isAddressIncluded = true;
+            this.isAddressRequired = data.required;
+          }
+        });
       },
       err => {
         console.log(err);
